4cf9007ca605b786c930bb1bd8f8cfe77da6a72e
martinfou <martinfou@gmail.com>
2017-07-15 22:18:09 -0400

    adding new element to the dictionary list

--- src/pages/dictionary/dictionary.ts
+++ src/pages/dictionary/dictionary.ts
@@ -22,6 +22,16 @@ export class DictionaryPage {
       id: '0',
       associatedWord: 'saw',
       associatedImage: 'assets/img/saw.jpg'
+    },
+    {
+      id: '1',
+      associatedWord: 'tea',
+      associatedImage: 'assets/img/tea.jpg'
+    },
+    {
+      id: '2',
+      associatedWord: 'honney',
+      associatedImage: 'assets/img/honney.jpg'
     }
   ]
 
